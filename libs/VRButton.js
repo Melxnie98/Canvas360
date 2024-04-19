1819e5f0a74c2506cbaf8352bc0a5c78facb8ba4
Melxnie98 <118276210+Melxnie98@users.noreply.github.com>
2024-04-19 14:53:37 +1200

    hi

--- libs/VRButton.js
+++ libs/VRButton.js
@@ -115,7 +115,7 @@ class VRButton {
 			element.style.padding = '12px 6px';
 			element.style.border = '1px solid #fff';
 			element.style.borderRadius = '4px';
-			element.style.background = 'rgba(0,0,0,0.1)';
+			element.style.background = '#C3B1E1';
 			element.style.color = '#fff';
 			element.style.font = 'normal 13px sans-serif';
 			element.style.textAlign = 'center';
